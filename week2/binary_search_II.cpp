4ae2229e4e4d8e9108311483070bfaeb8b8210d0
jai <jaianand5789@gmail.com>
2022-03-25 09:32:53 +0530

    week 2 program+ week 3 start

---
+++ week2/binary_search_II.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void binary_function_2(int l)
+{}
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int l;
+        cin>>l;
+        binary_function_2(l);
+    }
+
+}
