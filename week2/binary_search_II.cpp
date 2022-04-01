7b78ccf9d6523102cd3811bddf5e9dc01cfaacce
jai <jaianand5789@gmail.com>
2022-04-01 09:21:18 +0530

    files

--- week2/binary_search_II.cpp
+++ week2/binary_search_II.cpp
@@ -1,8 +1,24 @@
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
-void binary_function_2(int l)
-{}
+void binary_function_2(int size1)
+{
+    vector<int> arr(size1,0); //sorted array 
+    int a;
+    for (int i=0;i<arr.size();i++)
+    {
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i=0;i<size1-2;i++)
+    {
+        if (arr[i]+arr[i+1]==arr[i+2])
+        {
+            int count=0;
+            cout<<i<<" "<<i+1<<i+2;
+        }
+    }
+}
 int main()
 {
     int t;
@@ -13,5 +29,4 @@ int main()
         cin>>l;
         binary_function_2(l);
     }
-
 }
